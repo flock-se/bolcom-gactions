67cd7c9f0240db5063d81c10dc5bcee24f16a06a
Vincent de Bruijn <bruijnv@gmail.com>
2017-11-24 16:27:25 +0100

    Add body parser

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const bolcomFunction = require('./src/fulfillment/fulfillment.js').bolcomFunction;
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.get('/', bolcomFunction);
 
